3c3622b93ddcb1b42fd6b12a23cdf5a5615ecf7f
Timur Salakhow <tim.salah.1601@gmail.com>
2021-03-05 21:14:49 +0300

    Добавлен ввод размера массива и начало для сортировки вставками

---
+++ TimSort/TimSort/Sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+const int minrun = 32;
+
+
+void insertionSort(vector<int>* arr,int right,int left)
+{
+	for (unsigned int i = left+1; i < right; i++)
+	{
+		 
+	}
+}
+
+
+int main()
+{
+	unsigned int N;
+	cout << "Enter size of massive: ";
+	cin >> N;
+
+
+}
